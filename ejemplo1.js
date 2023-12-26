55daf3ef561b05ddccf2184478b594591aa45cce
Samuel Castro <dreift133@gmail.com>
2023-12-26 19:51:59 +0100

    añadiendo mas ejemplos

--- ejemplo1.js
+++ ejemplo1.js
@@ -3,15 +3,33 @@ const body = document.body;//acceso a los nodos superiores de esta manera solo s
                             //y para doctype
 console.log(body);
 
-const body2 = document.body.childNodes;//esta propiedad retorna todos los nodos hijos directo de un nodo padre en este caso body
+
+
+
+const body2 = document.body.childNodes;//acede a todos los nodos hijos de un nodo padre en este caso todos son hijos de body
 console.log(body2);
 
+const body3 = document.body.childNodes[1];//acede a uno de los nodos hijos de body en concreto
+console.log(body3);
 
-const body3 = document.body.childNodes[1];//retornSAa el nodo de la posicion elegifa y los hijos de estaASASAS
-console.log(body3.childNodes);
+const body4 = document.body.childNodes[1];//primero accede a uno de los nodos hijos de body, despues muestra los hijos de ese elemento en concreto
+console.log(body4.childNodes);
 
-const body4 = document.body.children;//me imprime nodos tipo elemento?? 
-console.log(body4);
-
-const body5 = document.body.firstChild; //devuelve el primer nodo de body
+const body5 = document.body.children;//devuelve nodos de tipo elemento? 
 console.log(body5);
+
+const body6 = document.body.firstChild;//primer nodo de un padre
+console.log(body6);
+
+const body7 = document.body.childNodes[1];
+console.log(body7.firstChild);//esta devolviendo el primer hijo del segundo hijo de body(en este caso el div)
+//devolvera text
+
+const body8 = document.body.lastChild;//devuelve el ultimo nodo del padre
+console.log(body8);
+
+const body9 = document.body.firstElementChild;
+console.log(body9);
+
+const body10 = document.body.lastElementChild;
+console.log(body10)
